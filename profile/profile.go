22a36060f0e29b8e26eaf7fcd339a5d4eeb8e2ca
g0j <sergkuz92@gmail.com>
2021-05-15 00:26:42 +0300

    extend profile description

--- profile/profile.go
+++ profile/profile.go
@@ -4,4 +4,13 @@ type Profile struct {
 	GUID  string
 	Name  string
 	Proxy string
+	User  User
+}
+
+type User struct {
+	Name     string
+	LastName string
+	Birthday string
+	Mobile   string
+	Email    string
 }
